b2ac64813b18ae2f82ad668677397738d04517e4
Yurii Perederii <yuriyperederiyyy@gmail.com>
2026-08-20 22:53:27 +0300

    LVL system setup

--- Entities/BaseHero.cs
+++ Entities/BaseHero.cs
@@ -7,8 +7,13 @@ using demo_rpg.GameSystem;
 
 namespace demo_rpg.Entities
 {
-    public abstract class BaseHero
+    public abstract class BaseHero : ILevelable
     {
+        public ushort CurrentLVL { get; protected set; } = 1;
+        public UInt32 CurrentEXP { get; protected set; } = 0u;
+        public UInt32 EXPtoNextLVL { get; protected set; } = 10;
+        public bool IsLevelable { get; protected set; } = false;
+
         public ushort BaseSTR { get; protected set; }
         public ushort BaseINT { get; protected set; }
         public ushort HPGrowth { get; protected set; }
@@ -26,6 +31,15 @@ namespace demo_rpg.Entities
             Stats = new StatBlock(strength: BaseSTR, intelligence: BaseINT);
         }
 
+        public virtual void GainEXP(UInt32 gainedEXP) 
+        {
+            CurrentEXP += gainedEXP;
+
+            if (CurrentEXP >= EXPtoNextLVL) 
+            {
+                IsLevelable = true;
+            }
+        }
 
     }
 }
---
+++ GameSystem/ILevelable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_rpg.GameSystem
+{
+    public interface ILevelable
+    {
+        ushort CurrentLVL { get; }
+        UInt32 CurrentEXP { get; }
+        UInt32 EXPtoNextLVL { get; }
+        bool IsLevelable { get; }
+
+        void GainEXP(UInt32 gainedEXP);
+    }
+}
